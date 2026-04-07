cf701939cfef1dfae39e68e7277270d01c2f92a9
IAMNOTGIVINGUMYNAME <bolajiinumi@gmail.com>
2026-04-07 18:05:40 +0100

    Create easter_assignment_turtle.py
    I tried to do the homework

---
+++ easter_assignment_turtle.py
@@ -0,0 +1,29 @@
+import turtle
+
+t = turtle.Turtle()
+t.speed(1)
+t.pensize(4)
+
+def draw_square():
+    t.penup()
+    t.goto(-150,0)
+    t.pendown()
+
+    t.goto(-50,0)
+    t.goto(-50,100)
+    t.goto(-150,100)
+    t.goto(-150,0)
+
+def draw_t():
+    t.penup
+      
+     
+    
+
+
+
+
+
+draw_square()
+turtle.done
+    
